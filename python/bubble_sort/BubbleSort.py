a774e68ed372d8b5c8a088d96991305aac4559f4
Daniel Melo <92686481+daniellmelo@users.noreply.github.com>
2023-03-01 14:56:38 -0300

    Refatoração do algoritmo da função bubble_sort()

--- python/bubble_sort/BubbleSort.py
+++ python/bubble_sort/BubbleSort.py
@@ -10,13 +10,12 @@ def bubble_sort(dados):
     count_swaps = 0
     for i in range(n - m):
         for j in range(n-1):
-            v1 = dados[j]
-            v2 = dados[j+1]
-            if v1 > v2:
-                count_swaps +=1
-                aux = v1
-                dados[j] = v2
-                dados[j + 1] = aux
+            if dados[j] > dados[j+1]:
+                dados[j], dados[j+1] = dados[j+1], dados[j]
+                count_swaps += 1
+                # aux = dados[j]
+                # dados[j] = dados[j+1]
+                # dados[j + 1] = aux
         m += 1
     print(f'>> Quantidade de trocas: {count_swaps}')
     return dados
