fdf5b76df8ea5a7d5eff4f93d1ce943566c7760f
Daniel Melo <92686481+daniellmelo@users.noreply.github.com>
2023-03-04 21:23:30 -0300

    Descrição da função insertion_sort()

--- python/insertion_sort/InsertionSort.py
+++ python/insertion_sort/InsertionSort.py
@@ -1,4 +1,10 @@
 def insertion_sort(data: list):
+    """
+        Retorna a lista ordenada em ordem crescente
+        :param lista: Lista que será ordenada
+        :return: Uma tupla contendo a lista ordenada e a quantidade de trocas (Swaps) que foram feitos
+        Complexidade no pior caso (a lista está em ordem decrescente): O(n²)
+        """
     count_swaps = 0
     n = len(data)
     for i in range(1, n):
@@ -6,7 +12,7 @@ def insertion_sort(data: list):
         j = i - 1
         while (key < data[j]) and (j >= 0):
             data[j+1] = data[j]
-            count_swaps+=1
+            count_swaps += 1
             j -= 1
         data[j+1] = key
     return data, count_swaps
